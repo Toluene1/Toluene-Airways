dfe9e9542b3318bca2fbcc59c12e21a63e968123
Toluene1 <adaramoti111@gmail.com>
2023-01-11 15:53:41 +0100

    added the newsletter section to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,39 @@ function App() {
         </div>{" "}
         <br />
         <br />
+        <div className="newsLetter">
+          <form className=" formNews">
+            <p className="fw-bold">
+              Never miss out on our daily news and Updates
+            </p>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control w-50 inputStyle"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div class="mb-3 form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1">
+                I would like to get updates from Toluene Airways
+              </label>
+            </div>
+            <button type="submit" class="buttonhome1">
+              Submit
+            </button>
+          </form>
+        </div>{" "}
+        <br />
+        <br />
         <div className="aboutUs">
           <div className="planTop text-center textColor">
             <h4>Plan your travel with confidence</h4>
