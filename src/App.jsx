a47b473c06ef0c9f32af03f66ace83b48dba2a05
Toluene1 <adaramoti111@gmail.com>
2023-01-13 14:14:53 +0100

    added the travel section to home page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <DefaultLayout>
       <main className="mt-5">
+        {/* Top Page */}
         <div className="homepageTop">
           <div>
             <h2 className="textHome1">
@@ -18,6 +19,103 @@ function App() {
         </div>{" "}
         <br />
         <br />
+        {/* Travel Places */}
+        <div>
+          <div className=" travelHeader">
+            <h3>Let's go places together</h3>
+            <p>
+              Discover the latest offers and news and start planning your next
+              trip with us.
+            </p>
+          </div>
+          <div className="travelPlaces">
+            <div className="travelDivs shadow">
+              <img
+                src="https://www.qatarairways.com/content/dam/images/renditions/horizontal-2/campaigns/nsw-europe/discover-qatar/48-hours-in-doha/h2-qatar-museum-043A9139-qnt.jpg"
+                alt=""
+                className="imgTravel1"
+              />
+              <div className="pWithin shadowDivs">
+                <p className="fs-4">Experience an unforgettable day in Qatar</p>
+                <p>
+                  Plan your next holiday in Qatar and discover an authentic
+                  Arabian treasure in the heart of the Gulf.
+                </p>{" "}
+                <a href="#">
+                  {" "}
+                  Book now <i className="bi bi-arrow-right-short iIcon"></i>
+                </a>
+              </div>
+            </div>
+            <div className="travelDivs d-none d-sm-block">
+              <div className="d-flex mb-3 shadow boxRadius">
+                <div>
+                  <img
+                    src="https://www.qatarairways.com/content/dam/images/renditions/horizontal-2/destinations/united-states/las-vegas/h2-usa-las-vegas-city.jpg"
+                    alt=""
+                    className="splitImg"
+                  />
+                </div>
+                <div className="ps-3 w-100 shadowDivs">
+                  <p className="pSplit">Fly to 230+ cities in the USA</p>
+                  <p>
+                    Great fares and seamless <br /> connections with our
+                    partners.
+                  </p>
+                  <a href="#">
+                    <span className="pSplitBook">
+                      Book now <i className="bi bi-arrow-right-short iIcon"></i>
+                    </span>
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex mb-3 shadow boxRadius">
+                <div>
+                  <img
+                    src="https://www.qatarairways.com/content/dam/images/renditions/horizontal-3/brand/qdf/h3-qdf-hia-stall-chanel.jpg"
+                    alt=""
+                    className="splitImg"
+                  />
+                </div>
+                <div className="ps-3  w-100 shadowDivs">
+                  <p className="pSplit">Shop and dine before you fly</p>
+                  <p>
+                    Great fares and seamless <br /> connections with our
+                    partners.
+                  </p>
+                  <a href="#">
+                    <span className="pSplitBook">
+                      Discover <i className="bi bi-arrow-right-short iIcon"></i>
+                    </span>
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex shadow boxRadius">
+                <div>
+                  <img
+                    src="https://www.qatarairways.com/content/dam/images/renditions/horizontal-2/miscellaneous/people/solo/h2-man-sitting-on-boat-pier.jpg"
+                    alt=""
+                    className="splitImg"
+                  />
+                </div>
+                <div className="ps-3  w-100 shadowDivs">
+                  <p className="pSplit">Explore the world with Cash</p>
+                  <p>
+                    Great fares and seamless <br /> connections with our
+                    partners.
+                  </p>
+                  <a href="#">
+                    <span className="pSplitBook">
+                      Discover more
+                      <i className="bi bi-arrow-right-short iIcon"></i>
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* NewsLetter */}
         <div className="newsLetter">
           <form className=" formNews">
             <p className="fw-bold">
@@ -49,6 +147,7 @@ function App() {
         </div>{" "}
         <br />
         <br />
+        {/* Footer */}
         <div className="aboutUs">
           <div className="planTop text-center textColor">
             <h4>Plan your travel with confidence</h4>
