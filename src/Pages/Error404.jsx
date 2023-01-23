d1ff087db166bce53512d281cfbbfcbe93426e0e
Toluene1 <adaramoti111@gmail.com>
2023-01-23 14:37:35 +0100

    changed the error image

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -3,7 +3,11 @@ const Error404 = () => {
   return (
     <main>
       <div className="text-center">
-        <img className="w-50" src="404.gif" alt="Error 404" />
+        <img
+          className="w-50 m-auto p-5"
+          src="https://www.twaino.com/wp-content/uploads/2022/06/error-404.png"
+          alt="Error 404"
+        />
       </div>
     </main>
   );
